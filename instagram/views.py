eea014fd45398694190890c08b776a1db404935c
Frank <kipfrankline@gmail.com>
2020-08-10 23:47:10 +0300

    Add function to signup

--- instagram/views.py
+++ instagram/views.py
@@ -1,5 +1,44 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, Http404
+from .models import Comments, Image, Profile, Likes
+from .forms import SignUpForm, ImageForm, CommentForm, ProfileForm
+# from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate
+
+from django.contrib.auth.decorators import login_required
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes, force_text
+from .tokens import account_activation_token
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
+
+from friendship.models import Friend, Follow, Block
+from friendship.exceptions import AlreadyExistsError
 # Create your views here.
-def welcome(request):
-    return render(request, 'navbar.html')
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.is_active = False
+            user.save()
+            current_site = get_current_site(request)
+            mail_subject = 'Activate Your Instagram Account'
+            message = render_to_string('registration/acc_active_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': account_activation_token.make_token(user),
+            })
+            to_email = form.cleaned_data.get('email')
+            email = EmailMessage(
+                        mail_subject, message, to=[to_email]
+            )
+            email.send()
+            return HttpResponse('Please confirm your email address to complete the registration')
+    else:
+        form = SignupForm()
+    return render(request, 'registration/register.html', {'form': form})
+
